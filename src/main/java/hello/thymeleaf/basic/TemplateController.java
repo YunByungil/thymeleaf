97ce290cc055a2e45699839136cf3159b1acefea
YunByungil <dbsquddlfz@naver.com>
2023-01-07 20:07:45 +0900

    (템플릿 레이아웃2) html 전체를 바꾸는 거고, ~{::title}는 태그를 파라미터로 넘기는 것임

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -13,9 +13,14 @@ public class TemplateController {
         return "template/fragment/fragmentMain";
     }
 
-    @GetMapping("/layout")
+    @GetMapping("/layout") // <head> 정보만 바꾸는 것임
     public String layout() {
         return "template/layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend") // extend는 <html>을 바꾸는 것이다.
+    public String layoutExtend() {
+        return "template/layoutExtend/layoutExtendMain";
     }
 
 
